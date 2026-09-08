4a76ae76359d2c5732924ff3c5168cbc9c19012b
Srinjoy-dev <srinjoyghosh6578@gmail.com>
2026-09-09 00:58:11 +0530

    completed most work in NAVBAR

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 //import './App.css'
 
+import Navbar from "./components/shared/Navbar"
+
 function App() {
 
   return (
     <>
-     <h1 className='text-red-500'>Lets build job portal</h1>
+     <Navbar/>
      </>
   )
 }
