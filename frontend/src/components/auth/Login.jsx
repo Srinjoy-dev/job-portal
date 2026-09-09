b364423036615fbf59f7c5378c9de8e5055e663f
Srinjoy-dev <srinjoyghosh6578@gmail.com>
2026-09-10 02:45:22 +0530

    finished the login and signup page data flow

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,28 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../shared/Navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
-import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { RadioGroup } from "../ui/radio-group";
 import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
+   const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  
+  const submitHandler = async(e) => {
+    e.preventDefault();
+   //we dont need to return form same as signup page coz we are not returning file
+    //api call
+    try {
+        const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+          headers:{
+            "Content-Type":"application/json"
+          },
+          withCredentials:true,
+        });
+        //if the request was successful and get a confirmation from backend then send a popup or toast
+        if(res.data.success) {
+          navigate("/");//send the user to home page if login is successful
+          toast.success(res.data.message);
+        }
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
+}
+
   return (
     <div>
       <Navbar />
       <div className="flex items-center justify-center max-w-7xl mx-auto">
         <form
-          action=""
+          onSubmit={submitHandler}
           className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
         >
           <h1 className="font-bold text-xl mb-5">Login</h1>
           <div className="my-2">
             <Label>Email</Label>
-            <Input type="email" placeholder="srinjoy@gmail.com" />
+            <Input
+              type="email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+              placeholder="srinjoy@gmail.com"
+            />
           </div>
           <div className="my-2">
             <Label>Password</Label>
-            <Input type="password" placeholder="srinjoy" />
+            <Input
+              type="password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
+              placeholder="srinjoy"
+            />
           </div>
           <div className="flex items-center justify-between">
             <RadioGroup className="flex items-center gap-4 my-5">
@@ -31,6 +75,8 @@ const Login = () => {
                   type="radio"
                   name="role"
                   value="student"
+                  checked={input.role === "student"}
+                  onChange={changeEventHandler}
                   className="cursor-pointer"
                 />
                 <Label htmlFor="r1">Student</Label>
@@ -40,6 +86,8 @@ const Login = () => {
                   type="radio"
                   name="role"
                   value="recruiter"
+                  checked={input.role === "recruiter"}
+                  onChange={changeEventHandler}
                   className="cursor-pointer"
                 />
                 <Label htmlFor="r2">Recruiter</Label>
@@ -49,7 +97,12 @@ const Login = () => {
           <Button type="submit" className="w-full my-4">
             Login
           </Button>
-          <span className="text-sm">Don't have an account? <Link to="/signup" className='text-blue-600'>Signup</Link></span>
+          <span className="text-sm">
+            Don't have an account?{" "}
+            <Link to="/signup" className="text-blue-600">
+              Signup
+            </Link>
+          </span>
         </form>
       </div>
     </div>
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,36 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../shared/Navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup } from "../ui/radio-group";
 import { Button } from "../ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { toast } from "../ui/toast";
 
 const Signup = () => {
+  const [input, setInput] = useState({
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: "",
+  });
+const navigate = useNavigate();
+
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] }); //get the 0th index value from the file why?
+  };
+
+const submitHandler = async(e) => {
+    e.preventDefault();
+    //packing every data we got from frontendd into a box so we can send it to backend
+    const formData = new FormData();
+    formData.append("fullname",input.fullName);
+    formData.append("email",input.email);
+    formData.append("phoneNumber",input.phoneNumber);
+    formData.append("password",input.password);
+    formData.append("role",input.role);
+    if(input.file) {
+      formData.append("file",input.file);
+    }
+    //api call
+    try {
+        const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+          headers:{
+            "Content-Type":"multipart/form-data"//tells the server that the data includes things like a file
+          },
+          withCredentials:true,//allows things like cookies to be sent along with the request
+        });
+        //if the request was successful and get a confirmation from backend then send a popup or toast
+        if(res.data.success) {
+          navigate("/login");//send the user to login page if its successful
+          toast.success(res.data.message);
+        }
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
+}
+
   return (
     <div>
       <Navbar />
       <div className="flex items-center justify-center max-w-7xl mx-auto">
         <form
-          action=""
+          onSubmit={submitHandler}
           className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
         >
           <h1 className="font-bold text-xl mb-5">Sign Up</h1>
           <div className="my-2">
             <Label>Full Name</Label>
-            <Input type="text" placeholder="srinjoy" />
+            <Input
+              type="text"
+              value={input.fullName}
+              name="fullName"
+              onChange={changeEventHandler}
+              placeholder="srinjoy"
+            />
           </div>
           <div className="my-2">
             <Label>Email</Label>
-            <Input type="email" placeholder="srinjoy@gmail.com" />
+            <Input
+              type="email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+              placeholder="srinjoy@gmail.com"
+            />
           </div>
           <div className="my-2">
             <Label>Phone Number</Label>
-            <Input type="text" placeholder="srinjoy" />
+            <Input
+              type="text"
+              value={input.phoneNumber}
+              name="phoneNumber"
+              onChange={changeEventHandler}
+              placeholder="8080808080"
+            />
           </div>
           <div className="my-2">
             <Label>Password</Label>
-            <Input type="password" placeholder="srinjoy" />
+            <Input
+              type="password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
+              placeholder="srinjoy"
+            />
           </div>
           <div className="flex items-center justify-between">
             <RadioGroup className="flex items-center gap-4 my-5">
@@ -39,6 +116,8 @@ const Signup = () => {
                   type="radio"
                   name="role"
                   value="student"
+                  checked={input.role === "student"}
+                  onChange={changeEventHandler}
                   className="cursor-pointer"
                 />
                 <Label htmlFor="r1">Student</Label>
@@ -48,6 +127,8 @@ const Signup = () => {
                   type="radio"
                   name="role"
                   value="recruiter"
+                  checked={input.role === "recruiter"}
+                  onChange={changeEventHandler}
                   className="cursor-pointer"
                 />
                 <Label htmlFor="r2">Recruiter</Label>
@@ -55,13 +136,22 @@ const Signup = () => {
             </RadioGroup>
             <div className="flex items-center gap-2">
               <Label>Profile</Label>
-              <Input accept="image/*" type="file" className="cursor-pointer" />
+              <Input 
+              accept="image/*" 
+              type="file"
+              onChange={changeFileHandler}
+              className="cursor-pointer" />
             </div>
           </div>
           <Button type="submit" className="w-full my-4">
             Signup
           </Button>
-          <span className="text-sm">Already have an account? <Link to="/login" className='text-blue-600'>Login</Link></span>
+          <span className="text-sm">
+            Already have an account?{" "}
+            <Link to="/login" className="text-blue-600">
+              Login
+            </Link>
+          </span>
         </form>
       </div>
     </div>
