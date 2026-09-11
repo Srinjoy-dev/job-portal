ffd762a66040eb0fc48b70ef4a92f1c380035d58
Srinjoy-dev <srinjoyghosh6578@gmail.com>
2026-09-11 23:59:34 +0530

    added user return in user controller so login page was not progressing

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -92,7 +92,7 @@ export const login = async(req,res)=> {
        return res.status(200).cookie("token", token, {maxAge:1*24*60*60*1000,httpOnly:true,
          sameSite:'strict'}).json({
             message:`Welcome back ${user.fullname}`,
-            user,
+            user,//this was missing fixed that
             success:true
          })
     } catch (error) {
