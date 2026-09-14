f4a8816c126b13ce739493aee4df01d6950898d9
Srinjoy-dev <srinjoyghosh6578@gmail.com>
2026-09-14 23:37:30 +0530

    did work in the update profile button

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./shared/Navbar";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
@@ -6,11 +6,13 @@ import { Contact, Mail, Pen } from "lucide-react";
 import { Badge } from "./ui/badge";
 import { Label } from "./ui/label";
 import AppliedJobTable from "./AppliedJobTable";
+import UpdateProfileDialog from "./UpdateProfileDialog";
 
 const skills = ["Html", "css", "Javascript", "Reactjs"];
-
+ const isResume = true;
 const Profile = () => {
-  const isResume = true;
+    const [open, setOpen] = useState(false);
+
   return (
     <div>
       <Navbar />
@@ -31,7 +33,7 @@ const Profile = () => {
               </p>
             </div>
           </div>
-          <Button className="text-right" variant="outline">
+          <Button onClick={()=> setOpen(true)} className="text-right" variant="outline">
             <Pen />
           </Button>
         </div>
@@ -69,6 +71,7 @@ const Profile = () => {
             {/* Applied Job Table */}
             <AppliedJobTable/>
         </div>
+        <UpdateProfileDialog open={open} setOpen={setOpen}/>
     </div>
   );
 };
